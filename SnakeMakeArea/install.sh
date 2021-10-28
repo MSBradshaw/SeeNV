ea1b02b49ba0aa42c68f684d4fbf18c8aa1ddae9
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2021-10-28 16:30:53 -0600

    created installation script (I think it is done, but needs to be tested)

---
+++ SnakeMakeArea/install.sh
@@ -0,0 +1,23 @@
+conda create --name cnviz2 --file Install/cnviz.env -y
+
+conda activate cnviz2
+
+conda install -c bioconda pysam -y
+conda install -c bioconda bedtools -y
+conda config --add channels bioconda  -y
+conda config --add channels conda-forge -y
+conda install samtools==1.11 -y
+conda install snakemake-minimal>=5.24.1 -y
+conda install -c bioconda mosdepth -y
+
+# find where the conda env bin is located
+conda_loc=$(which python)
+# strip the python on the end of that location
+conda_bin=$(dirname $conda_loc)
+# add cnviz's python script to the conda env bin
+cp bin/*.py $conda_bin
+
+# install gargs
+wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux
+chmod 770 gargs_linux
+mv gargs_linux $conda_bin/gargs
