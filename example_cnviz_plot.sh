3a9c1384f379284b4009d0d78bc2e0481e888de6
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2022-10-28 11:42:26 -0600

    clean up experimental ideas

--- example_cnviz_plot.sh
+++
@@ -1,8 +0,0 @@
-python seenv.py -p -i pos.samples  \
--s ../WES_TargetCoverage_v2.bed \
--c six_pos_calls.txt \
--o ProbandTestOut \
--r SmallNegPanelDB \
--g Homo_sapiens.GRCh37.82.genes.bed.gz \
--a ../gnomad_v2.1_sv.sites.bed.gz \
--t 50
--- p2_ecdf.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-probe_rpms = pickle.load(open('probe_rpms.pickle','rb'))
-probe_ecdfs = pickle.load(open('probe_ecdfs.pickle','rb'))
-
-print(probe_rpms)
-print(probe_ecdfs)
