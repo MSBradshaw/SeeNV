f8283e93c6762613fa92cd3df5ef7d13caf1d5d4
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2022-10-28 11:41:08 -0600

    moved example scripts into their own dir

---
+++ ExampleScripts/example_build_panel_db.sh
@@ -0,0 +1,5 @@
+./build_panel.sh \
+-i panel.samples \
+-p ../WES_TargetCoverage_v2.bed \
+-c ../all_calls.txt \
+-o PanelDB3
---
+++ ExampleScripts/example_command.sh
@@ -0,0 +1,1 @@
+seenv -p -i pos.samples -s Example/probes.bed -c Example/all_calls.txt -o OutPutDir -r NegPanel/ -g Example/hg19.genes.bed.gz -a gnomad_v2.1_sv.sites.bed.gz -t 64
