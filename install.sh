d62757b11137c1bc8b5c90e841180f5989db8145
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2022-12-01 13:46:03 -0700

    change install location

--- install.sh
+++ install.sh
@@ -10,20 +10,16 @@ conda install samtools==1.11 -y
 conda install snakemake-minimal>=5.24.1 -y
 conda install -c bioconda mosdepth -y
 
-# find where the conda env bin is located
-conda_loc=$(which python)
-# strip the python on the end of that location
-conda_bin=$(dirname $conda_loc)
 # add SeeNV's python script to the conda env bin
-cp bin/*.py $conda_bin
+cp bin/*.py $CONDA_PREFIX/bin
 
 # install gargs
 wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux
 chmod 770 gargs_linux
-mv gargs_linux $conda_bin/gargs
+mv gargs_linux $CONDA_PREFIX/bin/gargs
 
-cp bin/seenv.py $conda_bin/seenv
-cp bin/build_panel.snake $conda_bin/build_panel.snake
-cp bin/run_proband.snake $conda_bin/run_proband.snake
-cp panel_config.json $conda_bin/panel_config.json
-cp proband_config.json $conda_bin/proband_config.json
+cp bin/seenv.py $CONDA_PREFIX/bin/seenv
+cp bin/build_panel.snake $CONDA_PREFIX/bin/build_panel.snake
+cp bin/run_proband.snake $CONDA_PREFIX/bin/run_proband.snake
+cp panel_config.json $CONDA_PREFIX/bin/panel_config.json
+cp proband_config.json $CONDA_PREFIX/bin/proband_config.json
