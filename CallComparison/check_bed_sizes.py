72b0b0e3ff57c3790c6b78478b543c0cd5654766
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2024-03-12 13:13:11 -0600

    removed CallComparison dir

--- CallComparison/check_bed_sizes.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-small_ones = 0
-for line in open(sys.argv[1],'r'):
-    row = line.strip().split('\t')
-    if int(row[2]) - int(row[1]) < 1000:
-        small_ones += 1
-print(small_ones)
